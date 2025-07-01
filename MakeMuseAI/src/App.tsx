f5a8165c74be7f8b48ce9cebfecec1e8a0eb863a
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2025-07-01 17:07:31 -0400

    Start with backend
    thingy

--- MakeMuseAI/src/App.tsx
+++ MakeMuseAI/src/App.tsx
@@ -1,27 +1,17 @@
 import Message from "./message";
-import { useEffect, useState } from 'react';
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
-import ApiTest from "./components/ApiTest";
 
 function App() {
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
 
-  const [greeting, setGreeting] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/greet')
-      .then(res => res.json())
-      .then(data => setGreeting(data.message));
-  }, []);
-
   let thing = "hello";
   return (
     <div className="App">
       <Alert>
-        Hello <span></span>there
+        Helloss <span></span>there
       </Alert>
 
       <ListGroup
@@ -29,8 +19,6 @@ function App() {
         heading={"Citiess"}
         onSelectItem={handleSelectItem}
       />
-
-      <ApiTest />
     </div>
   );
 }
